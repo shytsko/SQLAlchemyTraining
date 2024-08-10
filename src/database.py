0734b9c367bfd5d3b49b53ddc9ddb815edd322c7
Vladimir Shytsko <shytsko.v@gmail.com>
2024-08-11 00:08:21 +0300

    Add alembic

--- src/database.py
+++ src/database.py
@@ -21,7 +21,7 @@ class Base(DeclarativeBase):
     pass
 
 
-async def create_all():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+# async def create_all():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import asyncio
-from src.database import session_factory, create_all
-from sqlalchemy import text
+from src.database import session_factory
 from models import FirstModel
 
 
@@ -13,9 +12,6 @@ async def main():
         session.add_all((data1, data2))
         await session.commit()
 
-    async with session_factory() as session:
-        res = await session.select(FirstModel).all()
-        print(res)
 
 if __name__ == '__main__':
     asyncio.run(main())
